661d8db840ff03844b24e9e9d494fec6d67ecd76
Ashutosh Pandey <uncreatedfool@gmail.com>
2021-01-29 18:33:42 +0530

    Commited

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import connectDB from "./config/db.js";
 import colors from "colors";
 import questionRoutes from "./routes/questionRoutes.js";
@@ -12,15 +13,25 @@ dotenv.config();
 app.use(express.json());
 
 connectDB();
-app.get("/", (req, res) => {
-  res.send("API IS RUNNING");
-});
+
 app.use("/api/quiz", quizRoutes);
 
 app.use("/api/question", questionRoutes);
 app.use("/api/users/", userRoutes);
 app.use(errorHandler);
 app.use(notFound);
+
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/quizmaker/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "quizmaker", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API IS RUNNING");
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log("SERVER STARTED"));
